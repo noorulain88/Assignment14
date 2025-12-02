8d0955cc95330ade543e5f067421490c192e47be
noorulain88 <noorulainain88@gmail.com>
2025-12-03 00:03:38 +0500

    assignmnent14

---
+++ program.js
@@ -0,0 +1,106 @@
+// console.log("Hello World")
+// program 01
+// 1. Write a JS program to replace the first occurrence of the word "apple" with "green apple" using the replace method.
+
+// var fruit="apple"
+// replaceValue=fruit.replace("apple","redApple");
+// console.log("I like",replaceValue)
+
+// progrma 02
+// 2. Write a JS program to replace all occurrences of the word "car" with "bike" 
+
+// var cars="cars"
+// var replaceCar=cars.replace("cars","bikes")
+// console.log("I like ",replaceCar)
+
+// program 03
+// 3. Write a JS program to replace all spaces in a string with hyphens.
+
+// var space=" "
+// var hyphens=space.replace(" ","-")
+// console.log("repcing value is",hyphens)
+
+// progrma 04
+// 4. Write a JS program to replace digits inside the string "price99" with "120".
+
+// var prize="99"
+// var newPrize=prize.replace("99","120")
+// console.log("THe replacement is" ,newPrize)
+
+
+// program 05
+// 5. Write a JS program to generate a random number between 1 and 10 using 
+
+//  var random=Math.floor(Math.random()*10)
+//  console.log("THe random numbers is ",random)
+
+// progrma 06
+// 6. Write a JS program to pick a random fruit from an array using Math.random and Math.floor.
+
+// var fruit1='apple'
+// var fruit2='orange'
+
+// var randomFruit=Math.round(Math.random()*1)
+// if(randomFruit==0){
+//     console.log("the first fruit name is",fruit1)
+// }
+// else{
+//        console.log(" the seconde fruit is " ,fruit2) 
+// }
+
+
+// program 07
+// 7. Write a JS program to take a floating number from the user and print its ceil value using Math.ceil.
+
+// var floating=prompt("enter any float number")
+// var newFloating=Math.ceil(floating)
+//  console.log("the floating number",newFloating)
+
+// program 08
+// 8. Write a JS program to take a floating number from the user and print its floor value using Math.floor.
+
+// var floating=prompt("enter any float number")
+// var newFloating=Math.floor(floating)
+//  console.log("the floating number",newFloating)
+
+// program 09
+// 9. Write a JS program to take a number and round it to the nearest integer using Math.round.
+
+// var round=8.9
+// var newRound=Math.round(round);
+// console.log("the round value is ",newRound)
+
+// program 10
+// 10. Write a JS program to create a function that prints the sum of two numbers using parameters and arguments.
+
+// var a=10
+// var b=30
+// function Sum (a,b){
+//     Sum=a+b
+//     console.log("THe sum of A and B is ",Sum)
+// }
+// Sum(a,b)
+
+// program 11
+// 11. Write a JS program to create a function that prints the square of a given number.
+
+// var number=5
+// function multiplication(number){
+//   multiplication=number*number
+//   console.log("the square of 5 is",multiplication)
+    
+// }
+// multiplication(number)
+
+// program 12
+// 12. Write a JS program to create a function that takes a sentence and replaces a user given word with another word.
+alert("you can change the word the word in para")
+var paragraph=("Biryani cooked in a special way.")
+var replace=prompt("put the word you want to change" )
+var value=prompt("put the you want to see")
+
+
+
+
+
+
